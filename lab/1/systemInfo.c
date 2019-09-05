a7d0c07ceab1be2559d7bb2250d4d71d73a4514f
Ashley Hendrickson <hendrash@mail.gvsu.edu>
2019-09-05 14:41:37 -0400

    lab1

--- lab/1/systemInfo.c
+++ lab/1/systemInfo.c
@@ -2,12 +2,12 @@
 #include<unistd.h>
 #include<sys/types.h>
 #include<pwd.h>
-
+#include<stdlib.h>
 
 int main(){
 	char *userName;
-	char *hostName ="";
 	char hostBuffer[1024];
+	char variable [20];
 	gethostname(hostBuffer, sizeof(hostBuffer));
 	userName=getlogin();
 	register uid_t userId=getuid();
@@ -19,6 +19,9 @@ int main(){
 	printf("host is: %s \n\n",hostBuffer);
 	for(int i=0;i<5;i++){
 	printf("entry is: %s \n", getpwent()->pw_name);}
-	printf("=======================================");
+	printf("environment variable desired?");
+	scanf("%s",variable);
+	printf("value is: %s \n", getenv(variable));
+	printf("=======================================\n");
 return 0;
 }
