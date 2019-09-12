b4838361b57982a3fae93a1135a8116fb1e49aeb
Muna Gigowski <munagigowski@gmail.com>
2019-09-12 15:27:00 -0400

    Update interrupts.c

--- lab/3/interrupts.c
+++ lab/3/interrupts.c
@@ -8,17 +8,40 @@ void sigHandler(int);
 int main(){
 
 pid_t pid;
+
 if((pid=fork()) < 0){
 	perror("forking error");
+} else if (pid) { //parent's code
+	printf("\nspawned child PID# %i\n", pid);
+	signal(SIGINT,sigHandler);
+	signal(SIGUSR1, sigHandler);
+	signal(SIGUSR2, sigHandler);
+	printf("waiting...");
+	sleep();
+} else { //child's code
+
+	while(1) {
+		//sleep for 1-5 random seconds
+		//int randomTime = rand() %5 + 1;
+		//sleep(randomTime);
+		
+		//TODO generate signals
+	}
+	
 }
-printf("\nspawned child PID# %i\n", pid);
-signal(SIGINT,sigHandler);
-pause();
+
 
 }
 
 void sigHandler(int sigNum){
-	sleep(1);
-	printf("\nAll processes ending\n");
-	exit(0);
+	
+	if(sigNum == 2) {
+		sleep(1);
+		printf("\nAll processes ending\n");
+		exit(0);
+	} else if (sigNum == 10) {
+		printf("received a SIGUSR1 signal")
+	} else if (sigNum == 12) {
+		printf("received a SIGUSR2 signal")
+	}
 }
