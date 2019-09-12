6b0343323e52e4437448a6dfb3c04b27c865bf5f
Ashley Hendrickson <hendrash@mail.gvsu.edu>
2019-09-12 15:12:09 -0400

    lab 3 Control-D works

--- lab/3/interrupts.c
+++ lab/3/interrupts.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+void sigHandler(int);
 
 int main(){
+
 pid_t pid;
 if((pid=fork()) < 0){
 	perror("forking error");
 }
-printf("%d",pid);
+printf("\nspawned child PID# %i\n", pid);
+signal(SIGINT,sigHandler);
+pause();
+
+}
+
+void sigHandler(int sigNum){
+	sleep(1);
+	printf("\nAll processes ending\n");
+	exit(0);
 }
