0c21c6cda3318b6358eb3f7fc9d476e5ea9e8e2b
Ashley Hendrickson <hendrash@mail.gvsu.edu>
2019-09-17 14:03:59 -0400

    signals are communicating

--- lab/3/interrupts.c
+++ lab/3/interrupts.c
@@ -8,34 +8,54 @@ void handler(int signal);
 void errorChecker();
 int i, pid1, pid2, status;
 int main(){
-// Installing signal handlers 
-if(signal(SIGUSR1, handler)==SIG_ERR){
-	printf("Unable to install a signal handler for SIGUSR1\n");
-}
-if(signal(SIGUSR2, handler)==SIG_ERR){
-	printf("Unable to install a signal handler for SIGUSR2\n");
+	// Installing signal handlers 
+	if(signal(SIGUSR1, handler)==SIG_ERR){
+		printf("Unable to install a signal handler for SIGUSR1\n");
+	}
+	if(signal(SIGUSR2, handler)==SIG_ERR){
+		printf("Unable to install a signal handler for SIGUSR2\n");
+	}
+	pid1=getpid();
+	printf("Parent pid=%d\n", pid1);
+	if(pid2=fork()==0)//child
+	{
+		printf("Child pid =%d\n", pid1);
+
+		printf("Child: sending parent SIGUSR1 ..........");
+		kill(pid1, SIGUSR1);
+
+		printf("Child: sending parent SIGUSR2 ..........");
+		kill(pid1, SIGUSR2);
+		wait( &status);
+		printf("Child: sending parent SIGUSR2 ..........");
+		kill(pid1, SIGUSR2);
+	}
+	else{//parent
+		wait(&status);
+		for(;;);
+	}
 }
 
-pid1=getpid();
-printf("Parent pid=%d\n ", pid1);
-if(pid2=fork()==0)
-{
-	printf("Child pid =%d\n", pid1);
-	printf("Child: sending parent SIGUSR1");
-	kill(pid1, SIGUSR1);
-	printf("Child: sending parent SIGUSR2");
-	kill(pid1, SIGUSR2);
-}
-else{
+void handler(int signo){
+	if(signo==SIGUSR1){
+		printf("received a SIGUSR1 signal %d \n", signo);
 
-	wait(&status);
-}
+		//Reinstalling handlers 
+		if(signal(SIGUSR1, handler)==SIG_ERR)
+			printf("Unable to install a signal handler for SIGUSR1\n");
 }
 
-void handler(int signal){
-if(signal==SIGUSR1){
-printf("siguser1 hihhihihihh\n");
+	if(signo==SIGUSR2){
+		printf("received a SIGUSR2 signal %d \n", signo);} 
+	//Reinstalling handlers 
+	if(signal(SIGUSR2, handler)==SIG_ERR)
+		printf("Unable to install a signal handler for SIGUSR2\n");
+
+
+	return;
 }
-if(signal==SIGUSR2)
-printf("siguser2 lololoololololol\n");
-}
+
+
+
+
+
