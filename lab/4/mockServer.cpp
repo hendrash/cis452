4cfa0f891dca8957152a0e507ab018e72d6faa1a
Ashley Hendrickson <hendrash@mail.gvsu.edu>
2019-09-19 14:54:34 -0400

    lab 4

--- lab/4/mockServer.cpp
+++ lab/4/mockServer.cpp
@@ -4,27 +4,30 @@
 #include<unistd.h>
 using namespace std;
 mutex mtx;
-int main(){
+string returnFile(string fname);
+void getFile();
 
+int main(){
+getFile();
 }
 
 void getFile(){
 	string fileName;
 	cout <<"Enter in a file name .... \n";
 	cin >> fileName;
-
+	cout <<	returnFile(fileName);	
 }
 
 string returnFile(string fname){
-if(rand()%11>7){
-int sleepTime=7+rand()%4;
-sleep(sleepTime);
-return fname;
-}
-else{
-	sleep(1);
-	return fname;
+	if(rand()%11>7){
+		int sleepTime=7+rand()%4;
+		sleep(sleepTime);
+		return fname;
+	}
+	else{
+		sleep(1);
+		return ("retrieved file " + fname+" from database successfully\n");
 
-}
+	}
 }
 
