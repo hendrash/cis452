83f5f741d5f8eb112b120b4fd6543af9a0b2f25a
Ashley Hendrickson <hendrash@mail.gvsu.edu>
2019-09-26 13:57:48 -0400

    part 4 complete

--- lab/5/examples/sample1.c
+++ lab/5/examples/sample1.c
@@ -4,18 +4,23 @@
 #include <sys/stat.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-
 #define FOO 4096
 
 int main ()
 {
    int shmId;
    char *shmPtr;
+   struct shmid_ds sharedMem;
 
+  
    if ((shmId = shmget (IPC_PRIVATE, FOO, IPC_CREAT|S_IRUSR|S_IWUSR)) < 0) {
       perror ("i can't get no..\n");
       exit (1);
    }
+
+   shmctl(shmId,IPC_STAT, &sharedMem); 
+   printf("The size of the shared memory segment is %lu\n ",sharedMem.shm_segsz);
+
    if ((shmPtr = shmat (shmId, 0, 0)) == (void*) -1) {
       perror ("can't attach\n");
       exit (1);
@@ -23,6 +28,7 @@ int main ()
    //modify the print statement in Sample Program 1 to determine the ID of the shared memory segment
    printf ("shared memory Id is %d\n", shmId);
    printf ("value a: %p\t value b: %p\n", (void *) shmPtr, (void *) shmPtr + FOO);
+   pause();
    if (shmdt (shmPtr) < 0) {
       perror ("just can't let go\n");
       exit (1);
