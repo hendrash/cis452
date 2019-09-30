ea62c407703a98803a20f623771d4f067283f8ec
Ashley Hendrickson <hendrash@mail.gvsu.edu>
2019-09-30 15:49:00 -0400

    Syntax fixed but Segmentation faulting

--- lab/5/readerProgram.cpp
+++ lab/5/readerProgram.cpp
@@ -21,37 +21,38 @@ typedef struct Dataset{
 Dataset* sharedMemory;
 const int shared_segment_size = sizeof(Dataset);
 
-void my_handler(int num);
+void my_handler(int shmid);
 
 int main(){
-	
+	int shmid; 
 	//set up sigHandler to receive ^C signal and call custom signal handler function
 	struct sigaction sigIntHandler;
 	sigIntHandler.sa_handler = my_handler;
-    sigemptyset(&sigIntHandler.sa_mask);
-    sigIntHandler.sa_flags = 0;
-    sigaction(SIGINT, &sigIntHandler, NULL);
-	
+	sigemptyset(&sigIntHandler.sa_mask);
+	sigIntHandler.sa_flags = 0;
+	sigaction(SIGINT, &sigIntHandler, NULL);
+
 	// ftok to generate unique key 
-    key_t key = ftok("shmfile",65);
-	
-    // shmget returns an identifier in shmid 
-    int shmid = shmget(key,shared_segment_size,IPC_CREAT); 
-	
+	key_t key = ftok("shmfile",65);
+
+	// shmget returns an identifier in shmid 
+	if(shmget(key,shared_segment_size,IPC_CREAT)){
+		perror("Failed to assign shmid");
+	} 
 	//Attach struct to shared memory
 	sharedMemory = (Dataset* ) shmat (shmid, 0, 0);
-	
+
 	bool myTurn = true;
 	while(1) {
-				
-		if(!sharedMemory.writerTurn && myTurn) {
-			printf("Data read from memory: %s\n",sharedMemory.userInput);
-			sharedMemory.numTimesRead = sharedMemory.numTimesRead + 1;			
-			if(sharedMemory.numTimesRead == 2) {
+
+		if(!sharedMemory->writerTurn && myTurn) {
+			printf("Data read from memory: %s\n",sharedMemory->userInput.c_str());
+			sharedMemory->numTimesRead = sharedMemory->numTimesRead + 1;			
+			if(sharedMemory->numTimesRead == 2) {
 				//Reset number of times read for next round
-				sharedMemory.numTimesRead = 0;
+				sharedMemory->numTimesRead = 0;
 				//Set turn back to writer so it can write again
-				sharedMemory.writerTurn = 1;
+				sharedMemory->writerTurn = 1;
 				myTurn = true;
 			}
 			myTurn = false;
@@ -59,15 +60,15 @@ int main(){
 	} 
 
 }
-	
-	
+
+
 //When user enters ^C, print final stats before exiting the program
-void my_handler(int num) {
-	
+void my_handler(int shmid) {
+
 	//detach from shared memory  
-    shmdt(sharedMemory); 
-    // destroy the shared memory 
-    shmctl(shmid,IPC_RMID,NULL);
+	shmdt(sharedMemory); 
+	// destroy the shared memory 
+	shmctl(shmid,IPC_RMID,NULL);
 	exit(0);
 }
 
--- lab/5/writerProgram.cpp
+++ lab/5/writerProgram.cpp
@@ -22,49 +22,47 @@ typedef struct Dataset{
 
 Dataset* sharedMemory;
 const int shared_segment_size = sizeof(Dataset);
-	
+
 void my_handler(int num);
 
 int main(){
-	
+	int shmid = 0;	
 	//set up sigHandler to receive ^C signal and call custom signal handler function
 	struct sigaction sigIntHandler;
 	sigIntHandler.sa_handler = my_handler;
-    sigemptyset(&sigIntHandler.sa_mask);
-    sigIntHandler.sa_flags = 0;
-    sigaction(SIGINT, &sigIntHandler, NULL);
-		
+	sigemptyset(&sigIntHandler.sa_mask);
+	sigIntHandler.sa_flags = 0;
+	sigaction(SIGINT, &sigIntHandler, NULL);
+
 	// ftok to generate unique key 
-    key_t key = ftok("shmfile",65);
-	
-    // shmget returns an identifier in shmid 
-    int shmid = shmget(key,shared_segment_size,IPC_CREAT); 
+	key_t key = ftok("shmfile",65);
+
+	// shmget returns an identifier in shmid 
+	if((shmid = shmget(key,shared_segment_size,IPC_CREAT))){
+		perror("Failed to assign shmid");
+	} 
 	
 	//Attach struct to shared memory
 	sharedMemory = (Dataset* ) shmat (shmid, 0, 0);
-		
-  
-    // shmat to attach to shared memory 
-    //char *str = (char*) shmat(shmid,(void*)0,0); 
 	
+	// shmat to attach to shared memory 
+	//char *str = (char*) shmat(shmid,(void*)0,0); 
+
 	while(1) {
 		//gets(str);
-		if(sharedMemory.writerTurn) {
+		if(sharedMemory->writerTurn) {
 			cout << "Please provide data to be written into shared memory: ";
-			cin >> sharedMemory.userInput;			
-			printf("Data written into memory: %s\n",sharedMemory.userInput); 
-			sharedMemory.writerTurn = 0;
+			cin >> sharedMemory->userInput;			
+			printf("Data written into memory: %s\n",sharedMemory->userInput.c_str()); 
+			sharedMemory->writerTurn = 0;
 		}
 	}
-
 }
-	
-	
+
 //When user enters ^C, print final stats before exiting the program
-void my_handler(int num) {
-	
-    //detach from shared memory  
-    shmdt(sharedMemory);
+void my_handler(int shmid) {
+	//detach from shared memory  
+	shmdt(sharedMemory);
 	exit(0);
 }
 
