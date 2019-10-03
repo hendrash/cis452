0bd014875e02759cecf40cfa1f6184410b2e13cf
Muna Gigowski <munagigowski@gmail.com>
2019-10-02 20:32:58 -0400

    Update readerProgram.cpp

--- lab/5/readerProgram.cpp
+++ lab/5/readerProgram.cpp
@@ -21,6 +21,7 @@ int main(){
 
 	Dataset* sharedMemory;
 	sharedMemory->writerTurn=0;
+	sharedMemory->numTimesRead = 0;
 	//Dataset* sharedMemory = &sharedMem;
 
 	int shmid; 
@@ -58,17 +59,17 @@ int main(){
 	bool myTurn = true;
 	while(1) {
 		if(sharedMemory->writerTurn==0 && myTurn) {
-		myTurn=false;	
+			myTurn=false;	
 			//printf("Data read from memory: %s\n",sharedMemory->userInput);
 			sharedMemory->numTimesRead = sharedMemory->numTimesRead + 1;			
-//			if(sharedMemory->numTimesRead == 2) {
+			if(sharedMemory->numTimesRead == 2) {
 				//Reset number of times read for next round
 				sharedMemory->numTimesRead = 0;
 				//Set turn back to writer so it can write again
 				printf("Other side: %s\n", sharedMemory->userInput);
 				sharedMemory->writerTurn = 1;
 				myTurn = true;
-			//}
+			}
 		//	myTurn = false;
 		}
 	} 
