c178fd77bf7aa71a18a7bd3101075c621fa95487
hendrash <hendrash@mail.gvsu.edu>
2019-10-24 16:33:33 -0400

    part2

--- lab/8/example/s1.c
+++ lab/8/example/s1.c
@@ -2,16 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SIZE 16
-
 int main()
 {
-    char *data1;
-
-    data1 = malloc (SIZE);
+    size_t len=16;
     printf ("Please input username: ");
-    scanf ("%s", data1);
-    printf ("you entered: %s\n", data1);
-    free (data1);
+    char c;
+    char* str=malloc(sizeof(char)*len);
+    len=0;
+    while(EOF!=(c=fgetc(stdin)) && c !='\n'){
+	    str[len++]=c;	    
+	    if(len%16==0)
+	    str=realloc(str, sizeof(char)*(len+16));
+    }
+    str[len++]='\0';
+    printf("you entered %s\n",str);	
+    free(str);
     return 0;
 }
