21ec0a7e80c28d31a56973867b0fd991a39de6bd
Muna Gigowski <munagigowski@gmail.com>
2019-11-14 14:37:42 -0500

    WORKING COMMIT, program functional

--- lab/12/lsProgram.c
+++ lab/12/lsProgram.c
@@ -4,7 +4,7 @@
 #include <sys/types.h>
 #include <errno.h>
 #include <dirent.h>
-
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
@@ -12,25 +12,32 @@ int main(int argc, char *argv[])
    struct stat statBuf;
    DIR *dirPtr;
    struct dirent *entryPtr;
+   char *filePathPtr;
+   char actualpath [4096+1];
    
     if (argc < 2) {
-      printf ("Usage: diractory name required\n");
+      printf ("Usage: directory name required\n");
       exit(1);
     }
+	
+	//Take user-specified directory and create an absolute path to it
+	if((filePathPtr = realpath(argv[1], actualpath)) == NULL) {
+	  printf ("Usage: realpath failed\n");
+      exit(1);
+	}
 
    //open stream in the specified directory for reading in data
-   dirPtr = opendir (argv[1]);
-
+   dirPtr = opendir (filePathPtr);
+   
    //loop through every file in the specified directory, and print out its statistics
    while ((entryPtr = readdir (dirPtr))) {
-     if (stat (entryPtr->d_name, &statBuf) < 0) {
+     if (stat (filePathPtr, &statBuf) < 0) {
         perror ("huh?  there is ");
         exit(1);
      } else {
-	   //printf ("value is: %u\n", S_ISREG(statBuf.st_mode));
+	   //print file statistics
 	   printf ("User ID: %u ", statBuf.st_uid);
 	   printf ("Group ID: %u ", statBuf.st_gid);
-	   //printf ("Size (bytes): %li", statBuf.st_size);
        printf ("Inode number: %li ", entryPtr->d_ino);
 	   printf ("%-20s\n", entryPtr->d_name);
      }
