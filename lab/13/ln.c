cf93ef7abca29df2b2f8f67553d3d4e05d13d0f2
Ashley Hendrickson <hendrash@eos10.cis.gvsu.edu>
2019-11-21 14:17:00 -0500

    done

---
+++ lab/13/ln.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <dirent.h>
+#include <string.h>
+#include <unistd.h>
+#include <unistd.h>
+int main(int argc, char *argv[])
+{ 	if(strcmp(argv[1], "-s")==0 ){
+	symlink(argv[2],argv[3]);	
+				 }
+else{
+link(argv[1], argv[2]);	}
+	return 0;
+}
