0608786a459b7dc54a961351459a4dfa0c56087a
Muna Gigowski <munagigowski@gmail.com>
2019-11-21 13:45:25 -0500

    first commit

---
+++ lab/13/linkProgram.c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <dirent.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+   
+   return 0;
+}
